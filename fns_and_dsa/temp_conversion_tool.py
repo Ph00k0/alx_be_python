38ba3bd6a8e8925a694d30876b7a02017f976fc3
root <Ph00k0>
2024-07-06 15:21:38 -0700

    removed variable freezing point

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -3,7 +3,6 @@
 # Define global conversion factors
 FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
-FREEZING_POINT_FAHRENHEIT =32
 
 def convert_to_celsius(fahrenheit):
     """Converts Fahrenheit to Celsius using the global conversion factor."""
